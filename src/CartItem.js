c5bb3e42608c4646704ccf366cf09ead55eb4f0a
Dhruv Sharma <dhruv999ddrr@gmail.com>
2023-01-10 13:26:15 -0500

    added on click listiner and cartItem state

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,7 +3,27 @@ import React from "react";
 
 //this is a class based component 
 class CartItem extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            price : 999,
+            title : 'Mobile Phone',
+            qty : 1,
+            img : ''
+        }
+    }
+
+    //arrow function will cause increaseQuantity to bind to class
+    increaseQuantity = () => {
+        console.log("this" , this.state)
+    }
+
     render(){
+
+        const {price , title , qty} = this.state;
+        //object destructuring
+
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -13,16 +33,25 @@ class CartItem extends React.Component{
                 <div className="right-block">
                     {/*this is the rightblock where we have info on the item*/ }
                     <div style={styles.cart_item_font}>
-                        Phone
+                        {title}
                     </div>
                     <div style={ {color:'gray'} }>
-                        Qty: 1
+                        Qty: {qty}
                     </div>
                     <div style={{color : 'gray'}}>
-                        999 USD
+                        {price} USD
                     </div>
                     <div className="cart-item-actions">
                         {/*Buttons*/ }
+                        <img 
+                            alt = "increase"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"
+                            onClick={this.increaseQuantity}
+                         />
+                        
+                        <img alt = "decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/2569/2569198.png" />
+                        <img alt = "delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/9332/9332345.png" />
                     </div>
                 </div>
             </div>
