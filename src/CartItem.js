8c89ebada37c8b2b0c8de41e52573c2afd405ac0
Dhruv Sharma <dhruv999ddrr@gmail.com>
2023-01-14 22:31:45 -0500

    added increase and decrease functionality to cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -16,7 +16,49 @@ class CartItem extends React.Component{
 
     //arrow function will cause increaseQuantity to bind to class
     increaseQuantity = () => {
-        console.log("this" , this.state)
+        
+        
+        /*this.setState(
+            //in this form this setState causes the component to re-render 
+            //on every call, if we have multiple calls, from first till last call
+            //the object passed is shallow-merged and only one render takes place
+            //this is called batching  
+            
+            //the state object will be shallow - merged with this object 
+            {
+                qty : this.state.qty + 1
+            }
+        )*/
+
+        //using callback to work with setState
+        //in this form since we are using prevState in callback, multiple
+        //calls are batched but all changes are added
+        //setState is asynchronus, so we have option to add a
+        //second argument as callback thet will take place after the setState 
+        //guaranteed 
+        this.setState((prevState)=>{
+            return {
+                qty : prevState.qty + 1
+            }
+        })
+
+        //console.log("this" , this.state)
+    }
+
+    decreaseQuantity = () =>{
+
+        //function to decrease the quantity
+        this.setState( (prevState)=>{
+            if(prevState.qty > 0){
+                return {
+                    qty : prevState.qty - 1
+                }
+            }
+            else{
+                return {}
+            }
+        
+        } )
     }
 
     render(){
@@ -50,7 +92,11 @@ class CartItem extends React.Component{
                             onClick={this.increaseQuantity}
                          />
                         
-                        <img alt = "decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/2569/2569198.png" />
+                        <img alt = "decrease"
+                         className="action-icons" 
+                         src="https://cdn-icons-png.flaticon.com/512/2569/2569198.png" 
+                         onClick={this.decreaseQuantity}
+                         />
                         <img alt = "delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/9332/9332345.png" />
                     </div>
                 </div>
